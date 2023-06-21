2b337f1b05257565a7d0e822b34ff086debb8c73
Isadora Guedes <isasoaresguedes@gmail.com>
2023-06-20 23:14:44 -0300

    Adiciona configuração de iluminação

--- Modulo4/Modulo4/Origem.cpp
+++ Modulo4/Modulo4/Origem.cpp
@@ -26,6 +26,8 @@ using namespace std;
 #include "stb_image.h"
 #include "Shader.h"
 
+#include "Mesh.h"
+
 //window configuration
 void key_callback(GLFWwindow* window, int key, int scancode, int action, int mode);
 void setupWindow(GLFWwindow*& window);
@@ -38,9 +40,12 @@ int setupGeometry();
 int loadTexture(string path);
 void readFromObj(string path);
 
+int loadSimpleOBJ(string filepath, int& nVerts, glm::vec3 color = glm::vec3(1.0, 0.0, 1.0));
+
 vector<GLfloat> totalvertices;
 vector<GLfloat> vertices;
 vector<GLfloat> textures;
+vector<GLfloat> normais;
 string mtlFilePath = "";
 string textureFilePath = "";
 
@@ -70,22 +75,48 @@ int main()
 
 	setupWindow(window);
 
+	int width, height;
+	glfwGetFramebufferSize(window, &width, &height);
+	glViewport(0, 0, width, height);
+
 	Shader shader("../shaders/sprite.vs", "../shaders/sprite.fs");
 
-	readFromObj("../../Arquivos/SuzanneTriTextured.obj");
-	readFromMtl("../../Arquivos/mtl/" + mtlFilePath);
+	//readFromObj("../../Arquivos/SuzanneTriTextured.obj");
+	//readFromMtl("../../Arquivos/mtl/" + mtlFilePath);
 
-	GLuint texID = loadTexture("../../Arquivos/textures/" + textureFilePath);
-	GLuint VAO = setupGeometry();
+	//GLuint texID = loadTexture("../../Arquivos/textures/" + textureFilePath);
+	//GLuint VAO = setupGeometry();
 
 	glUseProgram(shader.ID);
-	glUniform1i(glGetUniformLocation(shader.ID, "tex_buffer"), 0);
+	
+	//Matriz de view -- posição e orientação da câmera
+	glm::mat4 view = glm::lookAt(glm::vec3(0.0, 0.0, 3.0), glm::vec3(0.0, 0.0, 0.0), glm::vec3(0.0, 1.0, 0.0));
+	shader.setMat4("view", value_ptr(view));
 
-	glm::mat4 projection = glm::mat4(1);
-	projection = glm::rotate(projection, /*(GLfloat)glfwGetTime()*/glm::radians(-180.0f), glm::vec3(0.0f, 1.0f, 0.0f));
+	//Matriz de projeção perspectiva - definindo o volume de visualização (frustum)
+	glm::mat4 projection = glm::perspective(glm::radians(45.0f), (float)width / (float)height, 0.1f, 100.0f);
+	shader.setMat4("projection", glm::value_ptr(projection));
 
-	GLint projLoc = glGetUniformLocation(shader.ID, "projection");
-	glUniformMatrix4fv(projLoc, 1, false, glm::value_ptr(projection));
+	glEnable(GL_DEPTH_TEST);
+
+	int nVerts;
+	GLuint VAO = loadSimpleOBJ("../../Arquivos/suzanneTriLowPoly.obj", nVerts);
+
+	Mesh suzanne1;
+	suzanne1.initialize(VAO, nVerts, &shader);
+
+
+	//Definindo as propriedades do material da superficie
+	shader.setFloat("ka", 0.2);
+	shader.setFloat("kd", 0.5);
+	shader.setFloat("ks", 0.5);
+	shader.setFloat("q", 10.0);
+
+	//Definindo a fonte de luz pontual
+	shader.setVec3("lightPos", -2.0, 10.0, 2.0);
+	shader.setVec3("lightColor", 1.0, 1.0, 0.0);
+
+
 
 	while (!glfwWindowShouldClose(window))
 	{
@@ -103,21 +134,15 @@ int main()
 
 		glm::mat4 model = glm::mat4(1);
 		setupTransformacoes(model);
-		
+
 		GLint modelLoc = glGetUniformLocation(shader.ID, "model");
 		glUniformMatrix4fv(modelLoc, 1, false, glm::value_ptr(model));
 
-		glActiveTexture(GL_TEXTURE0);
-		glBindTexture(GL_TEXTURE_2D, texID);
+		shader.setFloat("q", 1.0);
+		suzanne1.update();
+		suzanne1.draw();
 
-		glBindVertexArray(VAO);
-
-		glDrawArrays(GL_TRIANGLES, 0, (vertices.size() / 3));
-
-		glBindVertexArray(0);
-
-		glBindTexture(GL_TEXTURE_2D, 0);
-
+		// Troca os buffers da tela
 		glfwSwapBuffers(window);
 	}
 
@@ -222,7 +247,7 @@ void readFromObj(string path) {
 					iss >> vertexIndex >> slash >> textIndex >> slash >> normalIndex;
 
 					glm::vec3 verticess = temp_vertices[vertexIndex - 1];
-					glm::vec3 normais = temp_normais[normalIndex - 1];
+					glm::vec3 normaiss = temp_normais[normalIndex - 1];
 					glm::vec2 texturess = temp_textures[textIndex - 1];
 
 					vertices.push_back(verticess.x);
@@ -231,6 +256,10 @@ void readFromObj(string path) {
 
 					textures.push_back(texturess.x);
 					textures.push_back(texturess.y);
+
+					normais.push_back(normaiss.x);
+					normais.push_back(normaiss.y);
+					normais.push_back(normaiss.z);
 				}
 			} 
 			else if (prefix == "mtllib")
@@ -288,10 +317,6 @@ int loadTexture(string path)
 void setupWindow(GLFWwindow*& window) {
 	glfwInit();
 
-	//glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
-	//glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
-	//glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-
 	window = glfwCreateWindow(WINDOW_WIDTH, WINDOW_HEIGHT, "Modulo 3 - Isadora Guedes", nullptr, nullptr);
 	glfwMakeContextCurrent(window);
 
@@ -307,10 +332,6 @@ void setupWindow(GLFWwindow*& window) {
 	const GLubyte* version = glGetString(GL_VERSION); /* version as a string */
 	cout << "Renderer: " << renderer << endl;
 	cout << "OpenGL version supported " << version << endl;
-
-	int width, height;
-	glfwGetFramebufferSize(window, &width, &height);
-	glViewport(0, 0, width, height);
 }
 
 void setupTransformacoes(glm::mat4& model) {
@@ -417,4 +438,161 @@ void resetAllRotate() {
 	rotateX = false;
 	rotateY = false;
 	rotateZ = false;
+}
+
+int loadSimpleOBJ(string filepath, int& nVerts, glm::vec3 color)
+{
+	vector <glm::vec3> vertices;
+	vector <GLuint> indices;
+	vector <glm::vec2> texCoords;
+	vector <glm::vec3> normals;
+	vector <GLfloat> vbuffer;
+
+	ifstream inputFile;
+	inputFile.open(filepath.c_str());
+	if (inputFile.is_open())
+	{
+		char line[100];
+		string sline;
+
+
+
+		while (!inputFile.eof())
+		{
+			inputFile.getline(line, 100);
+			sline = line;
+
+			string word;
+
+			istringstream ssline(line);
+			ssline >> word;
+
+			//cout << word << " ";
+			if (word == "v")
+			{
+				glm::vec3 v;
+
+				ssline >> v.x >> v.y >> v.z;
+
+				vertices.push_back(v);
+			}
+			if (word == "vt")
+			{
+				glm::vec2 vt;
+
+				ssline >> vt.s >> vt.t;
+
+				texCoords.push_back(vt);
+			}
+			if (word == "vn")
+			{
+				glm::vec3 vn;
+
+				ssline >> vn.x >> vn.y >> vn.z;
+
+				normals.push_back(vn);
+			}
+			if (word == "f")
+			{
+				string tokens[3];
+
+				ssline >> tokens[0] >> tokens[1] >> tokens[2];
+
+				for (int i = 0; i < 3; i++)
+				{
+					//Recuperando os indices de v
+					int pos = tokens[i].find("/");
+					string token = tokens[i].substr(0, pos);
+					int index = atoi(token.c_str()) - 1;
+					indices.push_back(index);
+
+					vbuffer.push_back(vertices[index].x);
+					vbuffer.push_back(vertices[index].y);
+					vbuffer.push_back(vertices[index].z);
+					vbuffer.push_back(color.r);
+					vbuffer.push_back(color.g);
+					vbuffer.push_back(color.b);
+
+					//Recuperando os indices de vts
+					tokens[i] = tokens[i].substr(pos + 1);
+					pos = tokens[i].find("/");
+					token = tokens[i].substr(0, pos);
+					index = atoi(token.c_str()) - 1;
+
+					vbuffer.push_back(texCoords[index].s);
+					vbuffer.push_back(texCoords[index].t);
+
+					//Recuperando os indices de vns
+					tokens[i] = tokens[i].substr(pos + 1);
+					index = atoi(tokens[i].c_str()) - 1;
+
+					vbuffer.push_back(normals[index].x);
+					vbuffer.push_back(normals[index].y);
+					vbuffer.push_back(normals[index].z);
+				}
+			}
+
+		}
+
+	}
+	else
+	{
+		cout << "Problema ao encontrar o arquivo " << filepath << endl;
+	}
+	inputFile.close();
+
+	GLuint VBO, VAO;
+
+	nVerts = vbuffer.size() / 11; //Provisório
+
+	//Geração do identificador do VBO
+	glGenBuffers(1, &VBO);
+
+	//Faz a conexão (vincula) do buffer como um buffer de array
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+
+	//Envia os dados do array de floats para o buffer da OpenGl
+	glBufferData(GL_ARRAY_BUFFER, vbuffer.size() * sizeof(GLfloat), vbuffer.data(), GL_STATIC_DRAW);
+
+	//Geração do identificador do VAO (Vertex Array Object)
+	glGenVertexArrays(1, &VAO);
+
+	// Vincula (bind) o VAO primeiro, e em seguida  conecta e seta o(s) buffer(s) de vértices
+	// e os ponteiros para os atributos 
+	glBindVertexArray(VAO);
+
+	//Para cada atributo do vertice, criamos um "AttribPointer" (ponteiro para o atributo), indicando: 
+	// Localização no shader * (a localização dos atributos devem ser correspondentes no layout especificado no vertex shader)
+	// Numero de valores que o atributo tem (por ex, 3 coordenadas xyz) 
+	// Tipo do dado
+	// Se está normalizado (entre zero e um)
+	// Tamanho em bytes 
+	// Deslocamento a partir do byte zero 
+
+	//Atributo posição (x, y, z)
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 11 * sizeof(GLfloat), (GLvoid*)0);
+	glEnableVertexAttribArray(0);
+
+	//Atributo cor (r, g, b)
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 11 * sizeof(GLfloat), (GLvoid*)(3 * sizeof(GLfloat)));
+	glEnableVertexAttribArray(1);
+
+	//Atributo coordenada de textura (s, t)
+	glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 11 * sizeof(GLfloat), (GLvoid*)(6 * sizeof(GLfloat)));
+	glEnableVertexAttribArray(2);
+
+	//Atributo normal do vértice (x, y, z)
+	glVertexAttribPointer(3, 3, GL_FLOAT, GL_FALSE, 11 * sizeof(GLfloat), (GLvoid*)(8 * sizeof(GLfloat)));
+	glEnableVertexAttribArray(3);
+
+
+	// Observe que isso é permitido, a chamada para glVertexAttribPointer registrou o VBO como o objeto de buffer de vértice 
+	// atualmente vinculado - para que depois possamos desvincular com segurança
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+	// Desvincula o VAO (é uma boa prática desvincular qualquer buffer ou array para evitar bugs medonhos)
+	glBindVertexArray(0);
+
+	return VAO;
+
 }
